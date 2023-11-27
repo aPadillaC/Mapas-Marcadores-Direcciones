4a75b1aa83f829822f5454f13a9787a21943ced3
Antonio Padilla <antoniopc88@gmail.com>
2023-11-27 11:38:15 +0100

    Geolocation

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+// Valida si el navegador tiene la Geolocalización activa o no
+if ( !navigator.geolocation){
+  alert('Navegador no soporta la Geolocalización');
+  throw new Error('Navegador no soporta la Geolocalización');
+}
+
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
