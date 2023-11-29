4f4805e53e8affca18536ece2458ac358b30ef1c
Antonio Padilla <antoniopc88@gmail.com>
2023-11-29 18:02:52 +0100

    Renderizar mapa

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
+import { Map } from 'mapbox-gl';
 
 
 @Component({
@@ -7,13 +8,35 @@ import { PlacesService } from '../../services';
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
 
-  constructor ( private placesService: PlacesService) {}
 
-  ngOnInit(): void {
-    console.log(this.placesService.userLocation);
+  // ViewChild permite acceder a elementos DOM nativos que tienen una variable de referencia de plantalla
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
 
+  constructor (
+    private placesService: PlacesService
+  ) {}
+
+
+
+  // Usamos el ngAlterViewInit para renderizar el mapa siempre
+  ngAfterViewInit(): void {
+
+    // Validamos que si no tenemos Localizacion no se muestre nada
+    if (!this.placesService.userLocation) {
+      throw Error('No hay placesService.userLocation');
+    }
+
+    // Código para mostrar el mapa
+    const map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.placesService.userLocation, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+    });
   }
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1IjoicGFkaTg4IiwiYSI6ImNsa2t6a245dTBlcWozY2w5dWR6eHhxbjMifQ.BFx7JnomOQA8k-NGgoCzZA';
+
+
 // Valida si el navegador tiene la Geolocalización activa o no
 if ( !navigator.geolocation){
   alert('Navegador no soporta la Geolocalización');
