c3f601faaea2d481e7d68ae643e8974000728053
Antonio Padilla <antoniopc88@gmail.com>
2023-12-04 16:38:27 +0100

    Debounce Manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SearchBarComponent {
 
+  // Variable para el debounce Manual
+  private debounceTimer?: NodeJS.Timeout;
+
+  constructor () {  }
+
+
+
+  // Metodo para realizar la petición cuando el usuario deja de teclear por 350 milesimas de segundos
+  onQueryChanged( query: string = '') {
+
+    if( this.debounceTimer ) clearTimeout( this.debounceTimer )
+
+    this.debounceTimer = setTimeout(() => {
+
+    }, 350)
+
+  }
 }
